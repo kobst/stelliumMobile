5a005dc97b75ca2cdaff65a32c78cbc5f29492ce
kobst <Edwrdhn@gmail.com>
2025-07-14 12:26:54 -0400

    Add user onboarding screen with birth chart form

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigator from './TabNavigator';
+import UserOnboardingScreen from '../screens/UserOnboardingScreen';
+import { useStore } from '../store';
 
 const Stack = createStackNavigator();
 
 const RootNavigator: React.FC = () => {
+  const { userData } = useStore();
+  const profileComplete = Boolean(
+    userData && userData.birthLocation && userData.timezone,
+  );
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -13,7 +20,11 @@ const RootNavigator: React.FC = () => {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Main" component={TabNavigator} />
+        {profileComplete ? (
+          <Stack.Screen name="Main" component={TabNavigator} />
+        ) : (
+          <Stack.Screen name="Onboarding" component={UserOnboardingScreen} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
